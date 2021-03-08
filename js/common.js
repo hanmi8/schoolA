05a3ca0e49b98f75e326d1f01b3f1493072aeaa3
hanmi8 <hanmi8080@gmail.com>
2021-03-08 23:07:29 +0900

    메인 사이트맵 퍼블리싱

--- js/common.js
+++ js/common.js
@@ -1,9 +1,7 @@
 $(function(){
     $('.container').css('min-height', $(window).height() - $('.topWrap').outerHeight() - $('.footer').outerHeight());
 
-    $('.hamburger').click(function(){
-        console.log('click')
-        $(this).toggleClass('on');
-        $('.sitemap').slideToggle(200);
+    $('.sitemapClose').click(function(){
+       $('.sitemap').hide();
     })
 })
